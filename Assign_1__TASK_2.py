e6a5df6c72db2d54d6a84bd5eb917daf50d63b5b
Gs_Sidhu <102853351+guru-desi@users.noreply.github.com>
2025-12-24 16:15:57 +0530

    Create Assign_1__TASK_2.py

---
+++ Assign_1__TASK_2.py
@@ -0,0 +1,15 @@
+# Assignment 1 Task 2  
+# Create a Personalized Greeting.
+
+# Define Variables For user Input First & Last Name 
+
+firstname = input("Enter your First Name : ")
+lastname = input("Enter your Last Name : ")
+
+#Concatenattion of First Name and Last Name 
+
+fullname = firstname + " " + lastname
+
+# Greatting Message 
+
+print(f"Hello , {fullname}! Welcome to the Python Program")
