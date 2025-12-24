585b05afc67c6b17549df7d1a53409a233c4e493
Gs_Sidhu <102853351+guru-desi@users.noreply.github.com>
2025-12-24 16:15:25 +0530

    Create Assign_1__TASK_1.py

---
+++ Assign_1__TASK_1.py
@@ -0,0 +1,20 @@
+# Assignment 1  Task 1 
+# Perform Basic Mathematical Operations 
+
+# Define variables for Input Numbers
+
+Num1 = int(input("Please Enter First Number : "))
+Num2 = int(input("Please Enter Second Number : "))
+
+#Define All Operations Here 
+sum = Num1+Num2
+subract = Num1-Num2
+multiplication = Num1*Num2
+division = Num1/Num2
+
+#Print On Screen Result 
+print("\n")
+print(f"Addition : ", sum)
+print("Subtraction : ", subract)
+print("Multiplication : ", multiplication)
+print("Division : ", division)
